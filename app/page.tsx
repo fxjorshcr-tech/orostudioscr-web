31b77ee1676e4c1bea5a6ab611a72574111d4012
grupooroztech <reportesorostudio@gmail.com>
2025-12-09 14:06:49 -0600

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-black">
+      <h1 className="text-4xl font-bold text-orange-500">
+        Hola OroStudiosCR 🎬
+      </h1>
+    </main>
+  );
+}
